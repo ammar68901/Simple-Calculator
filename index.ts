5882de7dbecab9ce55f0bb47fe61f3176ac661a4
Your Namecls <you@example.com>
2024-03-24 00:22:31 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import Choice from "inquirer/lib/objects/choice.js";
+
+const answer = await inquirer.prompt([
+  { message: "Enter Your first number", type: "number", name: "A" },
+  { message: "Enter Your second number", type: "number", name: "B" },
+  {
+    message: "what u choose to calculate",
+    type: "list",
+    choices: ["add", "sub", "mult", "divide"],
+    name: "operator",
+  },
+]);
+if (answer.operator === "add") {
+  console.log(answer.A + answer.B);
+} else if (answer.operator === "sub") {
+  console.log(answer.A - answer.B);
+} else if (answer.operator === "mult") {
+  console.log(answer.A * answer.B);
+} else if (answer.operator === "divide") {
+  console.log(answer.A / answer.B);
+} else {
+  ("the value is invalid");
+}
